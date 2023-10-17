82f93a66d9f5d4c76c20037acf7118ca65ae8548
Jordaan Mylonas <jordaanm@gmail.com>
2023-10-17 15:37:35 +1000

    fix: routing

--- src/App.tsx
+++ src/App.tsx
@@ -23,49 +23,49 @@ interface PageDatum {
 export const pageData: PageDatum[] = [
   {
     title: 'Home',
-    path: '/',
+    path: '',
     icon: 'hideout.png',
     element: <Home />
   },
   {
     title: 'Catalysts',
-    path: '/catalysts',
+    path: 'catalysts',
     icon: 'https://www.pathofexile.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvQ3VycmVuY3kvQ2F0YWx5c3RzL0ZlcnRpbGVDYXRhbHlzdCIsInciOjEsImgiOjEsInNjYWxlIjoxfV0/4b4ca5d929/FertileCatalyst.png',
     element: <Catalysts />
   },
   {
     title: 'Challenges',
-    path: '/challenges',
+    path: 'challenges',
     icon: 'challenge.png',
     element: <ChallengesPage />
   },
   {
     title: 'Delirium',
-    path: '/delirium',
+    path: 'delirium',
     icon: 'https://web.poecdn.com/image/Art/2DItems/Currency/Delirium/DeliriumOrbScarabs.png?w=1&h=1&scale=1',
     element: <DeliriumPage />
   },
   {
     title: 'Essences',
-    path: '/essences',
+    path: 'essences',
     icon: 'https://web.poecdn.com/image/Art/2DItems/Currency/Essence/Woe7.png?scale=1&w=1&h=1',
     element: <EssencesPage />
   },
   {
     title: 'Oils',
-    path: '/oils',
+    path: 'oils',
     icon: 'https://web.poecdn.com/image/blight/items/OpalescentOil.png?scale=1&w=1&h=1',
     element: <OilsPage />
   },
   {
     title: 'Scarabs',
-    path: '/scarabs',
+    path: 'scarabs',
     icon: 'https://web.poecdn.com/image/Art/2DItems/Currency/Scarabs/GreaterScarabBreach.png?scale=1&scaleIndex=0&w=1&h=1',
     element: <ScarabsPage />
   },
   {
     title: 'Settings',
-    path: '/settings',
+    path: 'settings',
     icon: 'settings.png',
     element: <SettingsPage />
   }
@@ -80,6 +80,7 @@ function App() {
           <Layout>
             <Routes>
               {pageData.map((page) => <Route key={page.title} path={page.path} element={page.element} />)}
+              {pageData.map((page) => <Route key={page.title} path={`/poe-helper/${page.path}`} element={page.element} />)}
             </Routes>
           </Layout>
         </BrowserRouter>
@@ -100,7 +101,7 @@ const Layout = (props: LayoutProps) => {
         <ul>
           {pageData.map((page) => (
             <li key={page.title} className="side-menu-item">
-              <Link to={page.path}>
+              <Link to={`/poe-helper/${page.path}`}>
                 <div className="icon">
                   <img src={page.icon} alt={`${page.title} icon`} aria-hidden />
                 </div>
